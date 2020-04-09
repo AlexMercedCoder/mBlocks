8f40565ad4ed7511a29b71a5cce85ee765b1232a
Alex Merced <alexmerced@alexmerced.com>
2020-04-09 14:53:42 -0400

    first commit

---
+++ block.js
@@ -0,0 +1,41 @@
+///////m-Blocks by Alex Merced of AlexMercedCoder.com
+
+//////////////
+//CaptureProps
+/////////////
+
+const captureProps = (element) => {
+    const att = [...element.attributes];
+    const entries = att.map((value) => {
+        return [value.name, value.value];
+    });
+
+    return Object.fromEntries(entries);
+};
+
+///////////////////
+//block class
+/////////////////
+class Block {
+    constructor(config) {
+        this.block = document.querySelector(`m-${config.name}#${config.id}`);
+        this.props = captureProps(this.block);
+        this.state = config.state;
+        this.rend();
+    }
+
+    rend() {
+        this.props = captureProps(this.block);
+        this.block.innerHTML = this.builder(this.state, this.props);
+        this.assemble();
+    }
+
+    builder(state, props) {}
+
+    assemble() {}
+
+    setState(newState) {
+        this.state = newState;
+        this.rend();
+    }
+}
